48fc554389b8d1cb4d237327339b33a8f6913eb3
Marcos Vasconcellos de Andrade <vascorj2006@gmail.com>
2019-08-08 16:36:55 -0300

    work8Ago19

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -5,7 +5,13 @@ const routes = require('./routes')
 
 const server = express()
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-fqk19.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true})
+//mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-fqk19.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true})
+
+mongoose.connect('mongodb://localhost/omnistack8', { useNewUrlParser: true })
+
+mongoose.Promise = global.Promise
+
+module.exports = mongoose
 
 server.use(express.json())
 
