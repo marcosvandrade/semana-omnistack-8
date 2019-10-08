a550ae38eb25af92a11a7faa25bb5acc90698499
marcosvandrade <vascorj2006@gmail.com>
2019-10-08 19:40:58 -0300

    home8out19

--- aulas/frontend/src/pages/Main.js
+++ aulas/frontend/src/pages/Main.js
@@ -1,5 +1,92 @@
 import React from 'react';
+import './Main.css'
 
-export default function Main() {
-    return <h1>Hello World</h1>
+import logo from '../assets/logo.svg'
+import dislike from '../assets/dislike.svg'
+import like from '../assets/like.svg'
+
+
+export default function Main({ match }) {
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Programador e cantor sertanejo nos tempos livres</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="dislike" />
+                        </button>
+                        
+                        <button type="button">
+                            <img src={like} alt="like" />
+                        </button>
+                    </div>
+
+                </li>
+
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Programador e cantor sertanejo nos tempos livres</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="dislike" />
+                        </button>
+                        
+                        <button type="button">
+                            <img src={like} alt="like" />
+                        </button>
+                    </div>
+
+                </li>
+
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Programador e cantor sertanejo nos tempos livres</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="dislike" />
+                        </button>
+                        
+                        <button type="button">
+                            <img src={like} alt="like" />
+                        </button>
+                    </div>
+
+                </li>
+
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Programador e cantor sertanejo nos tempos livres</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="dislike" />
+                        </button>
+                        
+                        <button type="button">
+                            <img src={like} alt="like" />
+                        </button>
+                    </div>
+
+                </li>
+            </ul>
+        </div>
+    )
 }
